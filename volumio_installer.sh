daae1dbd3887d2e55e947bd6722d77f039a20efb
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2018-03-12 10:15:22 +0100

    Clean up way more

--- volumio_installer.sh
+++ volumio_installer.sh
@@ -10,9 +10,17 @@ fi
 echo "Cloning github repo... (this might take a while)"
 git clone https://github.com/Saiyato/volumio-snapcast-plugin /home/volumio/volumio-snapcast-plugin
 
-echo "Cleaning up the directory for use"
+echo "Cleaning up the directory..."
 cd /home/volumio/volumio-snapcast-plugin
+rm -rf .git
+rm -rf images
+rm -rf known_working_versions
+rm .gitattributes
+rm brainstorm.txt
+rm readme.md
+rm snapcast.pptx
 rm volumio-snapcast-plugin.zip
 
 echo "Installing plugin..."
 volumio plugin install
+echo "Done!"
